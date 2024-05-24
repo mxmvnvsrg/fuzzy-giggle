cde429a74f865cb55e37aac3d0f6c782a6879c12
Max Ivanov <mxmvnvsrg@gmail.com>
2024-05-24 20:17:26 +0300

    feat: setup styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { CustomTextField } from '@/components/CustomInput';
+
 const App = () => {
   return (
     <div>
       <h1>Fuzzy-Giggle</h1>
+      <CustomTextField variant="outlined" />
     </div>
   );
 };
