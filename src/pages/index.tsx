1fe0ac6b8bc1c932fb20a873b8cd7b96c9b7a587
Max Ivanov <mxmvnvsrg@gmail.com>
2024-05-31 16:46:13 +0300

    feat: get pixabay images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import { CustomTextField } from '@/components/CustomInput';
+import { useEffect } from 'react';
+
+const API_URL =
+  'https://pixabay.com/api/?key=11626018-46d117711944bd4a59019ddb6';
 
 const App = () => {
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => console.log('data :>> ', data.hits));
+  }, []);
+
   return (
     <div>
       <h1>Fuzzy-Giggle</h1>
