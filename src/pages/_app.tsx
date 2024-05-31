b5812f1723e2c12f4bfdfa2b9b9815c81d945910
Maksym Ivanov <mxmvnvsrg@gmail.com>
2024-05-31 19:51:14 +0300

    feat: setup react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { JSX } from 'react';
 import { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,36 @@
 import { CustomTextField } from '@/components/CustomInput';
-import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
+// TODO: move to another place
 const API_URL =
   'https://pixabay.com/api/?key=11626018-46d117711944bd4a59019ddb6';
 
 const App = () => {
-  useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => console.log('data :>> ', data.hits));
-  }, []);
+  const { isPending, error, data } = useQuery({
+    queryKey: ['pixabayData'],
+    // TODO: make separate fn
+    // TODO: make async/await fn
+    queryFn: () => axios.get(API_URL),
+  });
+
+  if (isPending) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  console.log('data :>> ', data);
 
   return (
     <div>
       <h1>Fuzzy-Giggle</h1>
       <CustomTextField variant="outlined" />
+      <div>
+        {data.data.hits.map((item) => (
+          <div key={item.id}>
+            <img alt={`image-${item.id}`} src={item.previewURL} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
